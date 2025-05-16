d1b210fbaebb67dce74129575a06676d0a8674c7
kaibfang <kaibfang@qti.qualcomm.com>
2025-05-16 16:54:58 +0800

    NodeVoxelization: Add 1st version of Node Voxelization
    1. Add 1st version of Node Voxelization.
    2. Add 1st version of gtest_NodePointPillar.
    3. Add 1st version of SamplePlrPre for NodeVoxelization.
    
    Change-Id: I0ca05b375dbc3946f835cfa4d62a635ff9cc085c
    Signed-off-by: kaibfang <kaibfang@qti.qualcomm.com>

--- tests/sample/include/QC/sample/SamplePlrPre.hpp
+++ tests/sample/include/QC/sample/SamplePlrPre.hpp
@@ -2,54 +2,56 @@
 // All rights reserved.
 // Confidential and Proprietary - Qualcomm Technologies, Inc.
 
-
 #ifndef _QC_SAMPLE_PLRPRE_HPP_
 #define _QC_SAMPLE_PLRPRE_HPP_
 
-#include "QC/component/Voxelization.hpp"
+#include "QC/node/Voxelization.hpp"
 #include "QC/sample/SampleIF.hpp"
-
-using namespace QC;
-using namespace QC::component;
 
 namespace QC
 {
 namespace sample
 {
 
+using namespace QC::node;
+
 /// @brief qcnode::sample::SamplePlrPre
 ///
-/// SamplePlrPre that to demonstate how to use the QC component Voxelization
+/// SamplePlrPre that to demonstate how to use the QCNodeVoxelization
 class SamplePlrPre : public SampleIF
 {
 public:
     SamplePlrPre();
     ~SamplePlrPre();
 
-    /// @brief Initialize the Voxelization
+    /// @brief Initialize the voxelization
     /// @param name the sample unique instance name
     /// @param config the sample config key value map
     /// @return QC_STATUS_OK on success, others on failure
     QCStatus_e Init( std::string name, SampleConfig_t &config );
 
-    /// @brief Start the Voxelization
+    /// @brief Start the voxelization
     /// @return QC_STATUS_OK on success, others on failure
     QCStatus_e Start();
 
-    /// @brief Stop the Voxelization
+    /// @brief Stop the voxelization
     /// @return QC_STATUS_OK on success, others on failure
     QCStatus_e Stop();
 
-    /// @brief deinitialize the Voxelization
+    /// @brief deinitialize the voxelization
     /// @return QC_STATUS_OK on success, others on failure
     QCStatus_e Deinit();
 
 private:
-    QCStatus_e ParseConfig( SampleConfig_t &config );
     void ThreadMain();
+    QCStatus_e ParseConfig( SampleConfig_t &config );
 
 private:
-    Voxelization_Config_t m_config = { QC_PROCESSOR_HTP0, 0 };
+    QC::node::Voxelization m_voxel;
+    DataTree m_config;
+    DataTree m_dataTree;
+    QCProcessorType_e m_processor;
+
     uint32_t m_poolSize = 4;
 
     std::string m_inputTopicName;
@@ -59,14 +61,15 @@ private:
     SharedBufferPool m_coordsPool;
     SharedBufferPool m_featuresPool;
     bool m_stop;
+    int m_rsmPriority;
 
     DataSubscriber<DataFrames_t> m_sub;
     DataPublisher<DataFrames_t> m_pub;
 
-    Voxelization m_plrPre;
 };   // class SamplePlrPre
 
 }   // namespace sample
 }   // namespace QC
 
-#endif   // _QC_SAMPLE_PLRPRE_HPP_
+#endif   // _QC_SAMPLE_NODE_PLRPRE_HPP_
+
