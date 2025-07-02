c3a78ca8f523f4bf46554ca7f05bd3094bf465a0
Parai Wang <fpwang@qti.qualcomm.com>
2025-07-02 12:38:16 +0530

    QCNode: add temp Memory Utils to unlock the phase 2 dev
    This need to be updated to use QC Buffer Manager API instead.
    
    Change-Id: Ia1064634a37c4be9fd5753bb924786c2d0535ec2
    Signed-off-by: Parai Wang <fpwang@qti.qualcomm.com>

---
+++ source/Infras/Memory/Utils/BinaryAllocator.cpp
@@ -0,0 +1,106 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// All rights reserved.
+// Confidential and Proprietary - Qualcomm Technologies, Inc.
+
+#include "QC/Infras/Memory/Utils/BinaryAllocator.hpp"
+#include "QC/Infras/Memory/SharedBuffer.hpp"
+#include <unistd.h>
+
+namespace QC
+{
+namespace Memory
+{
+
+BinaryAllocator::BinaryAllocator( const std::string &name ) : QCMemoryAllocatorIfs( name )
+{
+    (void) QC_LOGGER_INIT( m_name.c_str(), LOGGER_LEVEL_ERROR );
+};
+
+BinaryAllocator::~BinaryAllocator()
+{
+    (void) QC_LOGGER_DEINIT();
+};
+
+QCStatus_e BinaryAllocator::Allocate( const QCBufferPropBase_t &request,
+                                      QCBufferDescriptorBase_t &response )
+{
+    QCStatus_e status = QC_STATUS_OK;
+    void *pData;
+    uint64_t dmaHandle;
+    QCBufferFlags_t flags = 0;
+    const BufferProps_t *pBufferProps = dynamic_cast<const BufferProps_t *>( &request );
+    BufferDescriptor_t *pBufferDescriptor = dynamic_cast<BufferDescriptor_t *>( &response );
+
+    if ( ( nullptr != pBufferProps ) && ( nullptr != pBufferDescriptor ) )
+    {
+        if ( QC_NON_CACHEABLE != request.attr )
+        {
+            flags |= QC_BUFFER_FLAGS_CACHE_WB_WA;
+        }
+
+        // TODO: replace flags with QCAllocationAttribute_e
+        // Do this at the end of phase 2 when to remove RideHal_SharedBuffer_t definition.
+        status =
+                QCDmaAllocate( &pData, &dmaHandle, pBufferProps->size, flags, pBufferProps->usage );
+        if ( QC_STATUS_OK == status )
+        {
+            pBufferDescriptor->name = m_name + "-" + std::to_string( dmaHandle );
+            pBufferDescriptor->pBuf = pData;
+            pBufferDescriptor->size = pBufferProps->size;
+            pBufferDescriptor->type = QC_BUFFER_TYPE_RAW;
+            pBufferDescriptor->pBufBase = pData;
+            pBufferDescriptor->dmaHandle = dmaHandle;
+            pBufferDescriptor->dmaSize = pBufferProps->size;
+            pBufferDescriptor->offset = 0;
+            pBufferDescriptor->id = 0; /* TODO: talk with Vadiam about the new buffer manager */
+            pBufferDescriptor->pid = static_cast<uint64_t>( getpid() );
+            pBufferDescriptor->usage = pBufferProps->usage;
+            pBufferDescriptor->attr = pBufferProps->attr;
+        }
+    }
+    else
+    {
+        status = QC_STATUS_BAD_ARGUMENTS;
+    }
+
+    return status;
+}
+
+QCStatus_e BinaryAllocator::Free( const QCBufferDescriptorBase_t &buffer )
+{
+    QCStatus_e status = QC_STATUS_OK;
+    const BufferDescriptor_t *pBufferDescriptor =
+            dynamic_cast<const BufferDescriptor_t *>( &buffer );
+    int pid = getpid();
+
+    if ( nullptr == pBufferDescriptor )
+    {
+        QC_ERROR( "buffer is invalid" );
+        status = QC_STATUS_INVALID_BUF;
+    }
+    else if ( nullptr == pBufferDescriptor->pBufBase )
+    {
+        QC_ERROR( "buffer not allocated" );
+        status = QC_STATUS_INVALID_BUF;
+    }
+    else if ( pid != pBufferDescriptor->pid )
+    {
+        QC_ERROR( "buffer not allocated by self, can't do free" );
+        status = QC_STATUS_OUT_OF_BOUND;
+    }
+    else
+    {
+        /* OK */
+    }
+
+    if ( QC_STATUS_OK == status )
+    {
+        status = QCDmaFree( pBufferDescriptor->pBufBase, pBufferDescriptor->dmaHandle,
+                            pBufferDescriptor->dmaSize );
+    }
+
+    return status;
+}
+}   // namespace Memory
+
+}   // namespace QC
---
+++ source/Infras/Memory/Utils/TensorAllocator.cpp
@@ -0,0 +1,117 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// All rights reserved.
+// Confidential and Proprietary - Qualcomm Technologies, Inc.
+
+#include "QC/Infras/Memory/Utils/TensorAllocator.hpp"
+#include "QC/Infras/Memory/SharedBuffer.hpp"
+#include <algorithm>
+#include <unistd.h>
+
+namespace QC
+{
+namespace Memory
+{
+
+#define SIZE_OF_FLOAT16 2
+static uint32_t s_qcTensorTypeToDataSize[QC_TENSOR_TYPE_MAX] = {
+        sizeof( int8_t ),  /* QC_TENSOR_TYPE_INT_8 */
+        sizeof( int16_t ), /* QC_TENSOR_TYPE_INT_16 */
+        sizeof( int32_t ), /* QC_TENSOR_TYPE_INT_32 */
+        sizeof( int64_t ), /* QC_TENSOR_TYPE_INT_64 */
+
+        sizeof( uint8_t ),  /* QC_TENSOR_TYPE_UINT_8 */
+        sizeof( uint16_t ), /* QC_TENSOR_TYPE_UINT_16 */
+        sizeof( uint32_t ), /* QC_TENSOR_TYPE_UINT_32 */
+        sizeof( uint64_t ), /* QC_TENSOR_TYPE_UINT_64 */
+
+        SIZE_OF_FLOAT16,  /* QC_TENSOR_TYPE_FLOAT_16 */
+        sizeof( float ),  /* QC_TENSOR_TYPE_FLOAT_32 */
+        sizeof( double ), /* QC_TENSOR_TYPE_FLOAT_64 */
+
+        sizeof( int8_t ),  /* QC_TENSOR_TYPE_SFIXED_POINT_8 */
+        sizeof( int16_t ), /* QC_TENSOR_TYPE_SFIXED_POINT_16 */
+        sizeof( int32_t ), /* QC_TENSOR_TYPE_SFIXED_POINT_32 */
+
+        sizeof( uint8_t ),  /* QC_TENSOR_TYPE_UFIXED_POINT_8 */
+        sizeof( uint16_t ), /* QC_TENSOR_TYPE_UFIXED_POINT_16 */
+        sizeof( uint32_t )  /* QC_TENSOR_TYPE_UFIXED_POINT_32 */
+};
+
+TensorAllocator::TensorAllocator( const std::string &name ) : BinaryAllocator( name )
+{
+    (void) QC_LOGGER_INIT( m_name.c_str(), LOGGER_LEVEL_ERROR );
+};
+
+TensorAllocator::~TensorAllocator()
+{
+    (void) QC_LOGGER_DEINIT();
+};
+
+QCStatus_e TensorAllocator::Allocate( const QCBufferPropBase_t &request,
+                                      QCBufferDescriptorBase_t &response )
+{
+    QCStatus_e status = QC_STATUS_OK;
+
+    const TensorProps_t *pTensorProps = dynamic_cast<const TensorProps_t *>( &request );
+    TensorDescriptor_t *pTensorDescriptor = dynamic_cast<TensorDescriptor_t *>( &response );
+
+    if ( ( nullptr != pTensorProps ) && ( nullptr != pTensorDescriptor ) )
+    {
+        BufferProps_t bufProps = *pTensorProps;
+        size_t size = 1;
+        uint32_t i = 0;
+
+        if ( ( pTensorProps->numDims > QC_NUM_TENSOR_DIMS ) ||
+             ( pTensorProps->tensorType >= QC_TENSOR_TYPE_MAX ) ||
+             ( pTensorProps->tensorType < QC_TENSOR_TYPE_INT_8 ) )
+        {
+            QC_ERROR( "pTensorProps has invalid numDims or type" );
+            status = QC_STATUS_BAD_ARGUMENTS;
+        }
+        else
+        {
+            /* check each dimension is reasonable */
+            for ( i = 0; i < pTensorProps->numDims; i++ )
+            {
+                if ( 0 == pTensorProps->dims[i] )
+                {
+                    QC_ERROR( "pTensorProps dims[%u] is 0", i );
+                    status = QC_STATUS_BAD_ARGUMENTS;
+                    break;
+                }
+            }
+        }
+
+        if ( QC_STATUS_OK == status )
+        {
+            for ( i = 0; i < pTensorProps->numDims; i++ )
+            {
+                size *= pTensorProps->dims[i];
+            }
+            size *= s_qcTensorTypeToDataSize[pTensorProps->tensorType];
+        }
+
+        if ( QC_STATUS_OK == status )
+        {
+            bufProps.size = size;
+            status = BinaryAllocator::Allocate( bufProps, response );
+            if ( QC_STATUS_OK == status )
+            {
+                pTensorDescriptor->type = QC_BUFFER_TYPE_TENSOR;
+                pTensorDescriptor->numDims = pTensorProps->numDims;
+                pTensorDescriptor->tensorType = pTensorProps->tensorType;
+                std::copy( pTensorProps->dims, pTensorProps->dims + pTensorProps->numDims,
+                           pTensorDescriptor->dims );
+            }
+        }
+    }
+    else
+    {
+        status = QC_STATUS_BAD_ARGUMENTS;
+    }
+
+    return status;
+}
+
+}   // namespace Memory
+}   // namespace QC
