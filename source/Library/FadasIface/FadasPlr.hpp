a77902110307236166977b06d688edb9f18bfed3
Parai Wang <fpwang@qti.qualcomm.com>
2025-08-14 13:57:11 +0530

    Component PostCenterPoint: move it as sample PostCenterPoint and use new buffer descriptor
    1. Move the Component PostCenterPoint as sample PostCenterPoint
    2. Update PostCenterPoint to use new buffer descriptor
    
    Change-Id: Id5caaf2abae4df77d51533b7cdb8deceb722e631
    Signed-off-by: Parai Wang <fpwang@qti.qualcomm.com>

--- source/Library/FadasIface/FadasPlr.hpp
+++ source/Library/FadasIface/FadasPlr.hpp
@@ -15,19 +15,6 @@ namespace libs
 namespace FadasIface
 {
 
-#ifdef __QNXNTO__
-typedef Fadas3DBBoxMetadata_t FadasPlr3DBBoxMetadata_t;
-#else
-/* The Ubuntu or Linux FadasVM mainline code dosn't support meanIntensity, with this workaround and
- * with engineer version library to support the meanIntensity. */
-typedef struct
-{
-    FadasPt_3Df32_t meanPt;
-    uint32_t numPts;
-    float32_t meanIntensity;
-} FadasPlr3DBBoxMetadata_t;
-#endif
-
 class FadasPlrPreProc : public FadasSrv
 {
 public:
@@ -85,91 +72,6 @@ private:
     uint32_t m_maxNumPtsPerPlr;
     uint32_t m_numOutFeatureDim;
 
-    bool m_bParamSet = false;
-};
-
-class FadasPlrPostProc : public FadasSrv
-{
-public:
-    FadasPlrPostProc();
-    ~FadasPlrPostProc();
-
-    QCStatus_e SetParams( float pillarXSize, float pillarYSize, float minXRange, float minYRange,
-                          float maxXRange, float maxYRange, uint32_t numClass, uint32_t maxNumInPts,
-                          uint32_t numInFeatureDim, uint32_t maxNumDetOut, float32_t threshScore,
-                          float32_t threshIOU, bool bMapPtsToBBox );
-
-    QCStatus_e SetFilterParams( float minCentreX, float minCentreY, float minCentreZ,
-                                float maxCentreX, float maxCentreY, float maxCentreZ,
-                                bool *labelSelect, uint32_t maxNumFilter );
-
-    QCStatus_e CreatePostProc();
-    QCStatus_e ExtractBBoxRun( const QCSharedBuffer_t *pHeatmap, const QCSharedBuffer_t *pXY,
-                               const QCSharedBuffer_t *pZ, const QCSharedBuffer_t *pSize,
-                               const QCSharedBuffer_t *pTheta, const QCSharedBuffer_t *pInPts,
-                               const QCSharedBuffer_t *pBBoxList, const QCSharedBuffer_t *pLabels,
-                               const QCSharedBuffer_t *pScores, const QCSharedBuffer_t *pMetadata,
-                               uint32_t *pNumDetOut );
-    QCStatus_e DestroyPostProc();
-
-private:
-    QCStatus_e CreatePostProcCPU();
-    QCStatus_e CreatePostProcDSP();
-
-    QCStatus_e ExtractBBoxRunCPU( const QCSharedBuffer_t *pHeatmap, const QCSharedBuffer_t *pXY,
-                                  const QCSharedBuffer_t *pZ, const QCSharedBuffer_t *pSize,
-                                  const QCSharedBuffer_t *pTheta, const QCSharedBuffer_t *pInPts,
-                                  const QCSharedBuffer_t *pBBoxList,
-                                  const QCSharedBuffer_t *pLabels, const QCSharedBuffer_t *pScores,
-                                  const QCSharedBuffer_t *pMetadata, uint32_t *pNumDetOut );
-    QCStatus_e ExtractBBoxRunDSP( const QCSharedBuffer_t *pHeatmap, const QCSharedBuffer_t *pXY,
-                                  const QCSharedBuffer_t *pZ, const QCSharedBuffer_t *pSize,
-                                  const QCSharedBuffer_t *pTheta, const QCSharedBuffer_t *pInPts,
-                                  const QCSharedBuffer_t *pBBoxList,
-                                  const QCSharedBuffer_t *pLabels, const QCSharedBuffer_t *pScores,
-                                  const QCSharedBuffer_t *pMetadata, uint32_t *pNumDetOut );
-
-    QCStatus_e DestroyPostProcCPU();
-    QCStatus_e DestroyPostProcDSP();
-
-
-private:
-    typedef union
-    {
-        void *hHandle;
-        uint64_t handle64;
-    } FadasPlrPostProc_PillarHandler_t;
-
-
-private:
-    remote_handle64 m_handle64;
-    FadasPlrPostProc_PillarHandler_t m_plrHandler;
-
-    float m_pillarXSize;
-    float m_pillarYSize;
-    float m_minXRange;
-    float m_minYRange;
-    float m_maxXRange;
-    float m_maxYRange;
-    uint32_t m_numClass;
-    uint32_t m_maxNumInPts;
-    uint32_t m_numInFeatureDim;
-    uint32_t m_maxNumDetOut;
-    float32_t m_threshScore;
-    float32_t m_threshIOU;
-    bool m_bMapPtsToBBox;
-
-    /* filter related parameters */
-    float m_minCentreX;
-    float m_minCentreY;
-    float m_minCentreZ;
-    float m_maxCentreX;
-    float m_maxCentreY;
-    float m_maxCentreZ;
-    uint32_t m_maxNumFilter;
-    std::vector<uint8_t> m_labelSelect;
-
-    bool m_bBBoxFilter = false;
     bool m_bParamSet = false;
 };
 
