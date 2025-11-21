973465d2c34a62ae058b16e6916aba65015f4ce4
Parai Wang <fpwang@qti.qualcomm.com>
2025-11-21 11:57:13 +0530

    Opticalflow test: add copy right header
    Change-Id: Iafdcb3a55ffe2c0541325b170e563977e1806398
    Signed-off-by: Parai Wang <fpwang@qti.qualcomm.com>

--- tests/unit_test/Node/OpticalFlow/gtest_OpticalFlow.cpp
+++ tests/unit_test/Node/OpticalFlow/gtest_OpticalFlow.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+// All rights reserved.
+// Confidential and Proprietary - Qualcomm Technologies, Inc.
 
 #include "md5_utils.hpp"
 #include "gtest/gtest.h"
